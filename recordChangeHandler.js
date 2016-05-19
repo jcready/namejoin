aad532d11f39cf5b58e9ab20eec17d05dc357bff
Bjorn Westergard <bjornw@gmail.com>
2016-05-18 21:35:23 -0400

    making destructuring match new format
    Signed-off-by: Bjorn Westergard <bjornw@gmail.com>

--- recordChangeHandler.js
+++ recordChangeHandler.js
@@ -1,25 +1,20 @@
 import API from './lib/api'
 
 export function handler (event, cb) {
-  console.log('event:', JSON.stringify(event, null, 2))
-  cb(null, event)
-  return
-}
+  const request = API(event)
+  const done = (e, res) => e ? cb(e) : cb(null, res)
+  const { record: { id, apiName }, priorState, changeSet } = event.lxMessage.message.contents
+  const wasNameChanged = 'first_name' in changeSet || 'last_name' in changeSet
+  if (!wasNameChanged) return done(null, 'No name change')
 
-  // const request = API(event)
-  // const done = (e, res) => e ? fail(e) : succeed(res)
-  // const { record: { id, apiName }, priorState, changeSet } = event.payload
-  // const wasNameChanged = 'first_name' in changeSet || 'last_name' in changeSet
-  // if (!wasNameChanged) return done(null, 'No name change')
-  //
-  // const { first_name, last_name } = { ...priorState, ...changeSet }
-  // const name = `${first_name} ${last_name}`.trim() || 'Unnamed Contact'
-  //
-  // console.log(`Changing name from "${priorState.name}" to "${name}" with post to /v1/records/${apiName}/${id}.`)
-  //
-  // return request({
-  //   method: 'PATCH',
-  //   path: `/v1/records/${apiName}/${id}`,
-  //   body: { name }
-  // }, done)
-// }
+  const { first_name, last_name } = { ...priorState, ...changeSet }
+  const name = `${first_name} ${last_name}`.trim() || 'Unnamed Contact'
+
+  console.log(`Changing name from "${priorState.name}" to "${name}" with post to /v1/records/${apiName}/${id}.`)
+
+  return request({
+    method: 'PATCH',
+    path: `/v1/records/${apiName}/${id}`,
+    body: { name }
+  }, done)
+}
