09a2889284966e4fb1f5eefd9d34bac4a8a1361d
Bjorn Westergard <bjornw@gmail.com>
2016-06-06 16:41:31 -0400

    parameterize fields
    Signed-off-by: Bjorn Westergard <bjornw@gmail.com>

--- recordChangeHandler.js
+++ recordChangeHandler.js
@@ -1,21 +1,32 @@
 import API from './lib/api'
+import { pipe, map, prop, join, trim, objOf, __ } from 'ramda'
+
+const determineName = (fieldsToJoin, record) =>
+pipe(
+    map(prop(__, record)),
+    join(' '),
+    trim
+)(fieldsToJoin)
 
 export function handler (event, cb) {
   console.log(`event: ${JSON.stringify(event, null, 2)}`)
+  
   const request = API(event)
   const done = (e, res) => e ? cb(e) : cb(null, res)
   const { record: { id, apiName }, priorState, changeSet } = event.lxMessage.message.contents
-  const wasNameChanged = 'first_name' in changeSet || 'last_name' in changeSet
-  if (!wasNameChanged) return done(null, 'No name change')
+  const { fields: { fieldToSet, fieldsToJoin } } = event.config
 
-  const { first_name, last_name } = { ...priorState, ...changeSet }
-  const name = `${first_name} ${last_name}`.trim() || 'Unnamed Contact'
+  const newName = determineName(fieldsToJoin, { ...priorState, ...changeSet })
 
-  console.log(`Changing name from "${priorState.name}" to "${name}" with post to /v1/records/${apiName}/${id}.`)
+  if (priorState.name !== newName) {
+    console.log(`Changing name from "${priorState.name}" to "${newName}" with post to /v1/records/${apiName}/${id}.`)
 
-  return request({
-    method: 'PATCH',
-    path: `/v1/records/${apiName}/${id}`,
-    body: { name }
-  }, done)
+    return request({
+      method: 'PATCH',
+      path: `/v1/records/${apiName}/${id}`,
+      body: objOf(fieldToSet, newName)
+    }, done)
+  } else {
+    cb(null, 'No change')
+  }
 }
