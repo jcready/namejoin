b74dcf0a67fd867fa6a4b1bec2524bb7b68ad72f
Wyatt Cready <jcready@gmail.com>
2016-06-29 09:59:58 -0400

    Update recordChangeHandler.js

--- recordChangeHandler.js
+++ recordChangeHandler.js
@@ -9,13 +9,13 @@ pipe(
 )(fieldsToJoin)
 
 export function handler (event, cb) {
-  console.log(`event: ${JSON.stringify(event, null, 2)}`)
+  console.log('event', event)
 
   const request = event.request
   const done = (e, res) => e ? cb(e) : cb(null, res)
   const { record: { id, apiName }, changeSet } = event.lxMessage.message.contents
   const priorState = event.lxMessage.message.contents.priorState || {}
-  const { fields: { fieldToSet, fieldsToJoin } } = event.config
+  const { fields: { fieldToSet, fieldsToJoin } } = event.parameters
 
   const newName = determineName(fieldsToJoin, { ...priorState, ...changeSet })
 
