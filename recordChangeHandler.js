e480f1d694305d63c71464fd13b3fde018c51d87
James Wyatt Cready <wyatt.cready@lanetix.com>
2016-07-01 15:06:34 -0400

    Get ready for scripting v3

--- recordChangeHandler.js
+++ recordChangeHandler.js
@@ -1,32 +1,37 @@
-import API from './lib/api'
 import { pipe, map, prop, join, trim, objOf, __ } from 'ramda'
-const determineName = (fieldsToJoin, record) =>
-pipe(
-    map(prop(__, record)),
-    join(' '),
-    trim
+const determineName = (fieldsToJoin, record) => pipe(
+  map(prop(__, record)),
+  join(' '),
+  trim
 )(fieldsToJoin)
 
-export function handler (event, cb) {
-  console.log('event', event)
-
-  const request = event.request
+export function handler ({
+  request,
+  parameters: {
+    fields: {
+      fieldToSet,
+      fieldsToJoin
+    }
+  },
+  lxMessage: {
+    message: {
+      contents: {
+        record: { id, apiName },
+        changeSet = {},
+        priorState = {}
+      }
+    }
+  }
+}, cb) {
   const done = (e, res) => e ? cb(e) : cb(null, res)
-  const { record: { id, apiName }, changeSet } = event.lxMessage.message.contents
-  const priorState = event.lxMessage.message.contents.priorState || {}
-  const { fields: { fieldToSet, fieldsToJoin } } = event.parameters
-
   const newName = determineName(fieldsToJoin, { ...priorState, ...changeSet })
-
   if (priorState.name !== newName) {
     console.log(`Changing name from "${priorState.name}" to "${newName}" with post to /v1/records/${apiName}/${id}.`)
-
-    /*return request({
+    request({
       method: 'PATCH',
       path: `/v1/records/${apiName}/${id}`,
       body: objOf(fieldToSet, newName)
-    }, done)*/
-    cb(null, 'Finished')
+    }, done)
   } else {
     cb(null, 'No change')
   }
